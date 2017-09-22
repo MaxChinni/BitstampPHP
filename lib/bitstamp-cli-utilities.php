2b0962df702f726b141fc1d3d5a5d0438e72288e
Massimiliano Chinni <max@mx17.net>
2017-09-22 14:22:22 +0200

    bitstamp-cli use a new library to parse arguments
    Add "vanilla/garden-cli" to manage command line arguments

--- lib/bitstamp-cli-utilities.php
+++ lib/bitstamp-cli-utilities.php
@@ -3,21 +3,3 @@ function echon($str = '')
 {
     echo "$str\n";
 }
-
-function printUsage()
-{
-    echon("Usage:");
-    echon("  ".basename(__FILE__)." <action>");
-    echon();
-    echon("actions:");
-    echon("  - ticker");
-    echon("  - tickerHour");
-    echon("  - orderBook");
-    echon("  - transactions");
-    echon("  - conversionRate: EUR/USD conversion rate");
-    echon("  - balance");
-    echon("  - openOrders");
-    echon("  - userTransactions");
-    echon("  - openOrders");
-    echon("  - cancelAllOrders");
-}
