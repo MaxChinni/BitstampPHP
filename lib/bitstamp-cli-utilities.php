6dd21a4edd8682ca6721eed293bd3968d0be152b
Massimiliano Chinni <max@mx17.net>
2017-09-22 15:40:50 +0200

    bitstamp-cli: buy all
    Now you can specify "all" to use all your balance to buy BTC at
    the price you specify, i.e.
    
        ./bitstamp-cli buy --amount all --price 2900

--- lib/bitstamp-cli-utilities.php
+++ lib/bitstamp-cli-utilities.php
@@ -3,3 +3,21 @@ function echon($str = '')
 {
     echo "$str\n";
 }
+
+function howManyBTCCanIBuyWithAllMyBalance($bitstamp, $price)
+{
+    $data = $bitstamp->balance();
+    // try to guess EUR vs USD
+    $yourCurrency = substr($bitstamp->getCurrency(), 3);
+    $available = $data["{$yourCurrency}_available"];
+    $feePercent = $data['fee'];
+    if ($available == 0) {
+        throw new \Exception("insufficient funds");
+    }
+    // Round fee to 2 decimals
+    $fee = ceil($available * ($feePercent / 100) * 100) / 100;
+    $available_for_buying = $available - $fee;
+    $btc = round($available_for_buying / $price, 8);
+
+    return $btc;
+}
